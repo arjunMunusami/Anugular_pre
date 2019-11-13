735dbdf0ba0a6eed3ec16fd26578f0ef47bb470b
Arjun <arjun.munusami1987@gmail.com>
2019-11-13 10:20:34 +0530

    changes related to design

--- TaskMgmt/src/app/view-task/view-task.component.ts
+++ TaskMgmt/src/app/view-task/view-task.component.ts
@@ -17,7 +17,6 @@ export class ViewTaskComponent implements OnInit {
   constructor() {
     // Create 100 users
     const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-    console.log(users);
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
   }
